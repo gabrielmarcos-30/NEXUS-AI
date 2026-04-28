8eca0c6608d465d18e631a115254b8847604b7fc
Bot Man 🌿 <gabrielmarcosrobas@gmail.com>
2026-04-28 17:16:25 +0100

    Atualizar o set.js

--- set.js
+++ set.js
@@ -14,9 +14,9 @@ const DATABASE_URL = process.env.DATABASE_URL === undefined
     : process.env.DATABASE_URL;
 module.exports = { session: process.env.SESSION_ID || '',
     PREFIXE: process.env.PREFIX || ".",
-    OWNER_NAME: process.env.OWNER_NAME || "",
+    OWNER_NAME: process.env.OWNER_NAME || "Ribery",
     NUMERO_OWNER : process.env.NUMERO_OWNER || " ",              
-    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
+    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "no",
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'no',
     BOT : process.env.BOT_NAME || 'NEXUS-AI',
     URL : process.env.BOT_MENU_LINKS || 'https://files.catbox.moe/e2rhpu.jpg',
